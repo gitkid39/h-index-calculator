ab71a5b8e5a0d8e4a5d0741f556cb508a4a4c737
gitkid39 <mscs16039@itu.edu.pk>
2019-09-11 17:01:00 +0500

    added to repo

---
+++ bingo.py
@@ -0,0 +1,28 @@
+import csv
+
+with open('citation.csv', 'rb') as csvfile:
+    data = list(csv.reader(csvfile))
+
+datacon = [[row[0],int(row[1])] for row in data]
+
+hindexlist = []
+datc = 0
+hindex = 0
+count = 0
+name=""
+for datum in datacon:
+    if datum[0] != name:
+        row = [name, hindex]
+        hindexlist.append(row)
+        hindex = 0
+        count = 0
+        name = datum[0]
+    count = count + 1
+    if count <= datum[1]:
+        hindex = hindex + 1
+        continue
+hindexlist = [[row[0],str(row[1])] for row in hindexlist]
+hindexlist.pop(0)
+with open('hindex.csv', 'wr') as hindexfile:
+    writer = csv.writer(hindexfile)
+    writer.writerows(hindexlist)
